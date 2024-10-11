aab309649dc91676da6595e92017b7c66f212c96
Bhushan-patil77 <145292155+Bhushan-patil77@users.noreply.github.com>
2024-10-12 00:52:42 +0530

    deployed backend url update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
 
 
-const socket = io('http://localhost:5000');
+const socket = io('https://chat-application-pi-five.vercel.app/');
 
 function App() {
     const [connectedUsers, setConnectedUsers] = useState({})
